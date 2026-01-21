1260df96aa0d5b25c1c8fb6fed90f78495876386
Kalindu-Karunarathna <kalindukarunarathne626@gmail.com>
2026-01-21 23:26:38 +0530

    useState practice

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,18 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import ProductCard from './components/product-card'
+
+import './index.css'
+import Test from './components/product-card'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-    <ProductCard name="kalindu" img="https://picsum.dev/400/300" price="1000.00"/>
-    <ProductCard name="chathuu" img="https://picsum.dev/400/300" price="2000.00"/>
-    <ProductCard name="kanaka" img="https://picsum.dev/400/300" price ="3000.00"/>
-    </>
-  )
+    <div className="w-[600px] h-[600px]  border border-amber-300 bg-blue-900">
+      <Test/>
+    </div>
+    
+    
+  );
 }
 
 export default App
--- src/components/product-card.jsx
+++ src/components/product-card.jsx
@@ -1,17 +1,37 @@
-export default function ProductCard(props){
+import { useState } from "react"
 
-    return(
+export default function Test(){
 
-        <div>
-            <h1>{props.name}</h1>    
-   
-    <img src={props.img} />
-    <p>price : {props.price}</p>
-   
-        </div>
-    
-   
+const [count,setcount] = useState(0)
 
-    );
+
+return(
+
+    <div className="w-full h-full bg-amber-200 flex justify-center items-center">
+
+        <button className="w-[200px] h-[70px] bg-amber-100" 
+        onClick={
+            ()=>{
+               setcount(count+1)
+            }
+        }>Increase</button>
+
+        <h1 className="w-[150px] h-[70px] text-center text-[40px]">{count}</h1>
+
+        <button className="w-[200px] h-[70px] bg-amber-100" 
+        onClick={
+            ()=>{
+                setcount(count-1)
+            }
+        }>Decrease</button>
+
+
+
+
+       
+    </div>
+)
+
+
 
 }
