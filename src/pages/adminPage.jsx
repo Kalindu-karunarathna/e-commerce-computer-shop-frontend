d9ea7ffe1a7188dbc3109ac9e84ce04978c68bcc
Kalindu-Karunarathna <kalindukarunarathne626@gmail.com>
2026-01-26 13:24:00 +0530

    create admin page and routes in admin page

--- src/pages/adminPage.jsx
+++ src/pages/adminPage.jsx
@@ -1,5 +1,34 @@
+import { Link, Route, Routes } from 'react-router-dom'
+
+
 export default function AdminPage(){
     return(
-        <h1>admin page</h1>
+        <div className="w-full h-full flex max-h-full bg-secondary">
+
+
+            <div className="w-[300px] h-full bg-secondary">
+                <div className="w-full h-[100px] flex items-center ">
+                    <img src="/logo.png" alt="logo" className="h-full" />
+                    <h1 className="text-3xl">Admin Pannel</h1>
+                </div>
+                <div className="w-full h-[400px] flex flex-col">
+                    <Link to="/admin">Orders</Link>
+                    <Link to="/admin/products">Products</Link>
+                    <Link to="/admin/users">Users</Link>
+                    <Link to="/admin/reviews">Reviews</Link>
+                </div>
+            </div>
+
+
+            <div className="w-[calc(100%-300px)] h-full bg-primary max-h-full overflow-y-auto border-[5px] border-secondary rounded-2xl ">
+               <Routes>
+                    <Route path="/" element={<h1>Orders</h1>}/>
+                    <Route path="/products" element={<h1>products</h1>}/>
+                    <Route path="/users" element={<h1>users</h1>}/>
+                    <Route path="/reviews" element={<h1>reviews</h1>}/>
+
+               </Routes>
+            </div>
+        </div>
     )
 }
--- src/pages/homePage.jsx
+++ src/pages/homePage.jsx
@@ -1,10 +1,19 @@
 import Header from "../components/header"
+import { Route, Routes } from 'react-router-dom'
 
 
 export default function HomePage(){
     return(
-        <div className="w-full h-full">
+        <div className="w-full h-full max-h-full overflow-y-auto">
            <Header/>
+           <div className="w-full min-h-[calc(100%-100px)] bg-primary">
+                <Routes>
+                    <Route path="/" element={<h1>Home page</h1>}/>
+                    <Route path="/products" element={<h1>Products</h1>}/>
+                    <Route path="/about" element={<h1>about</h1>}/>
+                    <Route path="/contact" element={<h1>contacts</h1>}/>
+                </Routes>
+           </div>
 
         </div>
     )
