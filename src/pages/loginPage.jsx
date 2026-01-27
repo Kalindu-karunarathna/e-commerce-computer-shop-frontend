0f3042c33f8e2e003deb2a0e9f7b3b5a841b74c5
Kalindu-Karunarathna <kalindukarunarathne626@gmail.com>
2026-01-27 16:21:11 +0530

    design login page

--- src/pages/loginPage.jsx
+++ src/pages/loginPage.jsx
@@ -1,5 +1,82 @@
+import { Link } from "react-router-dom";
+
 export default function LoginPage(){
     return(
-        <h1>login page</h1>
+        <div className="w-full h-screen bg-[url('/bg-login1.jpg')] bg-center bg-cover bg-no-repeat flex">
+
+
+
+            <div className="w-[50%] h-full flex items-center justify-center relative backdrop-blur-xs">
+
+                <div className="backdrop-blur-sm rounded-3xl px-14 py-12">
+                    <h1 className="
+                    text-4xl lg:text-5xl xl:text-6xl
+                    font-bold
+                    text-white
+                    tracking-wide
+                    drop-shadow-[0_4px_12px_rgba(255,255,255,0.25)]
+                    ">
+                    Smarter Tech Starts Here
+                    </h1>
+
+                    <p className="
+                    mt-3
+                    text-xl lg:text-2xl
+                    text-white/80
+                    font-light
+                    tracking-wider
+                    ">
+                    Premium computers, components, and accessories—delivered with confidence.
+                    </p>
+                </div>
+            </div>
+
+
+
+
+            <div className="w-[50%] h-full flex justify-center items-center ">
+                <div className="flex max-h-[80%] flex-col justify-center px-6 py-12 lg:px-8 w-[450px]  backdrop-blur-lg rounded-2xl
+                    shadow-[0_8px_30px_rgba(0,0,0,0.35)] hover:shadow-[0_15px_45px_rgba(0,0,0,0.45)] transition-shadow duration-300 ">
+                        
+                    <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+                        <img src="logo.png" alt="Your Company" className="mx-auto h-10 w-auto" />
+                        <h2 className="mt-10 text-center text-2xl/9 font-bold tracking-tight text-white">Log in to your account</h2>
+                    </div>
+
+                    <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+                        <form action="#" method="POST" className="space-y-6">
+                            <div>
+                                <label for="email" className="block text-sm/6 font-medium text-gray-100">Email address</label>
+                                <div className="mt-2">
+                                    <input id="email" type="email" placeholder="enter your email" name="email" required autocomplete="email" className="block p w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-white outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-350 focus:outline-2 focus:-outline-offset-2 focus:outline-secondary sm:text-sm/6" />
+                                </div>
+                            </div>
+
+                            <div>
+                                <div className="flex items-center justify-between">
+                                    <label for="password" className="block text-sm/6 font-medium text-gray-100">Password</label>
+                                    <div className="text-sm">
+                                        <Link to="forgot-password" className="font-semibold text-indigo-400 hover:text-indigo-300">Forgot password?</Link>
+                                    </div>
+                                </div>
+                                <div className="mt-2">
+                                    <input id="password" type="password" placeholder="enter your password" name="password" required autocomplete="current-password" className="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-white outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-350 focus:outline-2 focus:-outline-offset-2 focus:outline-secondary sm:text-sm/6" />
+                                </div>
+                            </div>
+
+                            <div>
+                                <button type="submit" className="flex w-full justify-center rounded-md bg-indigo-500 px-3 py-1.5 text-sm/6 font-semibold text-white hover:bg-indigo-400 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500">Log in</button>
+                            </div>
+                        </form>
+
+                        <p className="mt-10 text-center text-sm/6 text-gray-600">
+                            Don't have an account?
+                            <Link to="/register" className="font-semibold text-accent hover:text-indigo-400"> Sign up here</Link>
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+        </div>
     )
 }
